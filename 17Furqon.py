5369e4725de78eb25734803bfa611e3563c83aa5
muhammadfurqon2073 <94501696+muhammadfurqon2073@users.noreply.github.com>
2021-11-17 09:07:36 +0700

    Add files via upload

---
+++ 17Furqon.py
@@ -0,0 +1,16 @@
+Nama ="Muhammad Furqon"
+NIM=212410102073
+Asal="Banyuwangi"
+Hobby="Beternak kelinci"
+Fakultas="Ilmu Komputer"
+Jurusan="Teknologi Informasi"
+Instansi="Universitas Jember"
+
+print("Halo,Di sini saya akan memperkenalkan diri saya")
+print("Nama saya", Nama)
+print("Dengan NIM",NIM)
+print("Berasal dari",Asal)
+print("Hobby ku",Hobby)
+print("Saya kuliah di",Instansi)
+print("Fakultas",Fakultas)
+print("Jurusan",Jurusan)
